51708d713861b327d61d7722ea57d7f3328861f5
Joe Tustin <j.tustin@gmail.com>
2019-03-12 15:52:08 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,8 +11,8 @@ def loop_iterator(number_of_times)
 
   i = 0
   loop do
-  puts = "Welcome to Flatiron School's Web Development Course!"
-  i+=
+    puts = "Welcome to Flatiron School's Web Development Course!"
+    i = i + 1
     if i = number_of_times
       break
     end
